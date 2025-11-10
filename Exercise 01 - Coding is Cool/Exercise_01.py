96a75cb4e8333991b7105583990ba28523e5704b
Johndoe32124 <qpk924501@gmail.com>
2025-11-10 06:10:01 +0000

    Create Exercise_01.py

---
+++ Exercise 01 - Coding is Cool/Exercise_01.py
@@ -0,0 +1,8 @@
+#Exercise 1
+word1 = "Coding"
+word2 =  "is"
+word3 = "cool"
+# Use string concatenation to combine the variables and print the phrase
+print(f"{word1} {word2} {word3}")
+
+#Done on October 8th
