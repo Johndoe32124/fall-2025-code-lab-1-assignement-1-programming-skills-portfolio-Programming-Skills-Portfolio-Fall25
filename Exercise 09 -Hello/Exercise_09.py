070d96c75ded735b60a453f61c5536cb1824e0d8
Johndoe32124 <qpk924501@gmail.com>
2025-11-10 06:12:58 +0000

    Create Exercise_09.py

---
+++ Exercise 09 -Hello/Exercise_09.py
@@ -0,0 +1,14 @@
+#Exercise 9
+def hello():
+    print("Hello")
+    #prints hello
+
+def main():
+    hello()
+    #calls the hello function
+
+if __name__ == "__main__":
+	main()
+	#calls the main function
+
+#Done on October 8th
