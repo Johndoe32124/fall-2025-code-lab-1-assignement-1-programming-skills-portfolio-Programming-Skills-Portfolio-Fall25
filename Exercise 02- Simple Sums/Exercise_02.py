6329537120d778e09d72671ac8e66569198c2be7
Johndoe32124 <qpk924501@gmail.com>
2025-11-10 06:11:06 +0000

    Create Exercise_02.py

---
+++ Exercise 02- Simple Sums/Exercise_02.py
@@ -0,0 +1,7 @@
+#Exercise 2
+first_variable = 8
+second_variable = 10
+third_variable = 8 + 10
+print(f"the sum of {first_variable} and {second_variable} is {third_variable}")
+
+#Done on October 8th
