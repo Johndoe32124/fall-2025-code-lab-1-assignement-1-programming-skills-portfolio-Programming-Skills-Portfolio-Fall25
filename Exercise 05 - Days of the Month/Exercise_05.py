83ee30675f1e732c89d69294fb56a194c0377b68
Johndoe32124 <qpk924501@gmail.com>
2025-11-11 12:00:06 +0400

    Update Exercise_05.py

--- Exercise 05 - Days of the Month/Exercise_05.py
+++ Exercise 05 - Days of the Month/Exercise_05.py
@@ -1,20 +1,18 @@
-import calendar
+import calendar, os, sys
 from datetime import datetime
 
-import os, sys
-project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-dependencies_path = os.path.join(project_dir, "Dependencies")
-sys.path.append(dependencies_path)
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "Dependencies"))
 import Helper
 
 #Exercise 5
 def get_days_in_month(year, month=1):
-	while month < 1 or month > 12: month = Helper.input("Insert a month number between 1 and 12 ", input_types=(int,))
+	month = Helper.input("Insert a month number between 1 and 12 ", input_types=(int,), min_value=1, max_value=12)
 	days = calendar.monthrange(year, month)[1]
 	return days
 
 year = Helper.input("Insert a year number", input_types=(int,))
 month = Helper.input("Insert a month number ", input_types=(int,), min_value=1, max_value=12)
 print(f"Number of days in {calendar.month_name[month]} {year}: {get_days_in_month(year, month)}")
+
 #Done on October 8th
 #Automatically adjusts for leap years so incase human error occurs for example you enter 2025 as a leap year and it agreeing and just returning wrong info, it just does it for you without any extra input!
