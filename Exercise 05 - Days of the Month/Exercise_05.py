883bb36bf0391922995729ba7d8f5542607a91f0
Johndoe32124 <qpk924501@gmail.com>
2025-11-11 20:34:01 +0400

    Update Exercise_05.py

--- Exercise 05 - Days of the Month/Exercise_05.py
+++ Exercise 05 - Days of the Month/Exercise_05.py
@@ -5,11 +5,13 @@ import Helper
 
 #Exercise 5
 def get_days_in_month(year, month=1):
-	return calendar.monthrange(year, month)[1]
+	return calendar.month_name[month], calendar.monthrange(year, month)[1]
 
 year = Helper.input("Insert a year number: ", input_types=(int,))
 month = Helper.input("Insert a month number: ", input_types=(int,), min_value=1, max_value=12)
-print(f"Number of days in {calendar.month_name[month]} {year}: {get_days_in_month(year, month)}")
+name, days = get_days_in_month(year, month)
+
+print(f"Number of days in {name} {year}: {days}")
 
 #Done on October 8th
 #Automatically adjusts for leap years so incase human error occurs for example you enter 2025 as a leap year and it agreeing and just returning wrong info, it just does it for you without any extra input!
