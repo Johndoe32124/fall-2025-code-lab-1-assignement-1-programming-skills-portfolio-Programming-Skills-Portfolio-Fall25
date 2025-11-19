27610f39a4aea02d500da82ef673a99a051daf21
Johndoe32124 <qpk924501@gmail.com>
2025-11-19 17:03:51 +0400

    Update Exercise_03.py

--- Exercise 03 - Biography/Exercise_03.py
+++ Exercise 03 - Biography/Exercise_03.py
@@ -1,13 +1,14 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "Dependencies"))
 import Helper
-# ^^^^^ Imports instead of using the same code over and over you'll see this in the other exercises that require user input'
+# ^^^^^ Imports instead of using the same code over and over you'll see this in the other exercises that require user input
 
 #Exercise 3
-name = Helper.input("What is your name? ", input_types=(str,))
+first_name = Helper.input("What is your first name? ", input_types=(str,))
+second_name = Helper.input("What is your second name? ", input_types=(str,))
 age = Helper.input("How old are you? ", input_types=(int,))
 hometown = Helper.input("What is the name of your hometown? ", input_types=(str,))
 
-print(f"Name: {name}\nAge: {age}\nHome Town: {hometown}")
+print(f"Name: {first_name} {second_name}\nAge: {age}\nHome Town: {hometown}")
 
 #Done on October 8th
